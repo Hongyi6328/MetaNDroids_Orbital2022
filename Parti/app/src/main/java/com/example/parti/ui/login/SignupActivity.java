9a6489fbf42b1ded002985110a6a527c61d916ec
Felix Huang <6328huang@gmail.com>
2022-06-20 22:25:30 +0800

    a critical bug: failed to inflate fragmentContainerView

--- Parti/app/src/main/java/com/example/parti/ui/login/SignupActivity.java
+++ Parti/app/src/main/java/com/example/parti/ui/login/SignupActivity.java
@@ -3,12 +3,13 @@ package com.example.parti.ui.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.R;
 
 public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        //setContentView(R.); TODO
     }
 }
