b8fd9cb952e6531b43be2cef80592e392087387d
Felix Huang <6328huang@gmail.com>
2022-06-23 22:28:15 +0800

    working on firestore adapter

--- Parti/app/src/main/java/com/example/parti/ui/main/BrowseProjectsFragment.java
+++ Parti/app/src/main/java/com/example/parti/ui/main/BrowseProjectsFragment.java
@@ -1,35 +1,52 @@
 package com.example.parti.ui.main;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.text.HtmlCompat;
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.parti.R;
+import com.example.parti.databinding.FragmentBrowseProjectsBinding;
+import com.example.parti.recyclerview.BrowseProjectsAdapter;
 import com.example.parti.recyclerview.BrowseProjectsRecyclerViewListAdapter;
 import com.example.parti.wrapper.classes.Project;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.WriteBatch;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
-public class BrowseProjectsFragment extends Fragment {
+public class BrowseProjectsFragment extends Fragment implements BrowseProjectsAdapter.OnProjectSelectedListener {
 
-    FirebaseFirestore db;
+    FirebaseFirestore firebaseFirestore;
+    Query query;
+    BrowseProjectsAdapter browseProjectsAdapter;
+    FragmentBrowseProjectsBinding browseProjectsFragmentBinding;
+
     static final String TAG = "read-data";
 
     public BrowseProjectsFragment() {}
@@ -45,30 +62,22 @@ public class BrowseProjectsFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
-        db = FirebaseFirestore.getInstance();
-        List<Project> projectList = new ArrayList<>();
-        db.collection("projects")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d(TAG, document.getId() + " => " + document.getData());
-                                //Map<String, Object> map = document.getData();
-                                //Project nextProject = new Project(document.getId(), map.getOrDefault("title", "Empty Title").toString(), map.getOrDefault("description", "Empty Description").toString(), android.R.drawable.ic_dialog_email);
-                                //projectList.add(nextProject);
-                            }
-                        } else {
-                            Log.d(TAG, "Error getting documents: ", task.getException());
-                        }
-                    }
-                });
+        //setHasOptionsMenu(true);
+        browseProjectsFragmentBinding = FragmentBrowseProjectsBinding.inflate(inflater, container, false);
+        return browseProjectsFragmentBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        query = firebaseFirestore.collection("projects");
+        //.orderBy("avgRating", Query.Direction.DESCENDING)
+        //.limit(LIMIT);
+        browseProjectsAdapter = new BrowseProjectsAdapter(query, this);
 
         //int a = android.R.drawable.ic_dialog_email;
         //Log.d(TAG, "" + a);
 
-        Project[] projects = projectList.toArray(new Project[0]);
         /*
         // The following code is just for testing purposes
         Project[] projects = new Project[] {
@@ -87,6 +96,7 @@ public class BrowseProjectsFragment extends Fragment {
         };
          */
 
+        /*
         View view = inflater.inflate(R.layout.fragment_browse_projects, container, false);
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.browse_projects_recycler_view);
         BrowseProjectsRecyclerViewListAdapter adapter = new BrowseProjectsRecyclerViewListAdapter(projects);
@@ -94,5 +104,37 @@ public class BrowseProjectsFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
         return view;
+         */
+        browseProjectsFragmentBinding.browseProjectsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        browseProjectsFragmentBinding.browseProjectsRecyclerView.setAdapter(browseProjectsAdapter);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Start listening for Firestore updates
+        if (browseProjectsAdapter != null) {
+            browseProjectsAdapter.startListening();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (browseProjectsAdapter != null) {
+            browseProjectsAdapter.stopListening();
+        }
+    }
+
+
+    @Override
+    public void onProjectSelected(DocumentSnapshot project) {
+        // Go to the details page for the selected restaurant TODO
+        //BrowseProjectsFragmentDirections.ActionMainFragmentToRestaurantDetailFragment action = MainFragmentDirections
+        //        .actionMainFragmentToRestaurantDetailFragment(restaurant.getId());
+
+        //NavHostFragment.findNavController(this)
+        //        .navigate(action);
     }
 }
