581f33ad8d96344fb7bc396af92867995219d43f
Felix Huang <6328huang@gmail.com>
2022-07-08 15:47:08 +0800

    Update EditProjectActivity.java

--- Parti/app/src/main/java/com/example/parti/ui/main/EditProjectActivity.java
+++ Parti/app/src/main/java/com/example/parti/ui/main/EditProjectActivity.java
@@ -1,7 +1,13 @@
 package com.example.parti.ui.main;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,11 +16,15 @@ import com.example.parti.Parti;
 import com.example.parti.databinding.ActivityEditProjectBinding;
 import com.example.parti.databinding.ActivityViewProjectBinding;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Locale;
 
 public class EditProjectActivity extends AppCompatActivity {
 
     public enum Purpose {UPDATE, CREATE}
+
+    private static final int PICK_IMAGE = 1010;
 
     private ActivityEditProjectBinding activityEditProjectBinding;
     private Purpose purpose;
@@ -37,6 +47,22 @@ public class EditProjectActivity extends AppCompatActivity {
         if (extras != null) purpose = (Purpose) extras.get("purpose");
         if (purpose == Purpose.CREATE) setCreatePurpose();
         else if (purpose == Purpose.UPDATE) setUpdatePurpose();
+
+        activityEditProjectBinding.projectImageBig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                getIntent.setType("image/*");
+
+                Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                pickIntent.setType("image/*");
+
+                Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+                startActivityForResult(chooserIntent, PICK_IMAGE); //TODO
+            }
+        });
     }
 
     protected void setCreatePurpose() {
@@ -63,7 +89,7 @@ public class EditProjectActivity extends AppCompatActivity {
         activityEditProjectBinding.buttonSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                if (!validateInput()) return;
             }
         });
     }
@@ -75,5 +101,64 @@ public class EditProjectActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE) { //pick an image from local gallery or remote resources and show it
+            if (resultCode != Activity.RESULT_OK || data == null) {
+                Toast.makeText(this, "Failed to Pick Image", Toast.LENGTH_LONG).show();
+                return;
+            }
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                activityEditProjectBinding.projectImageBig.setImageBitmap(selectedImage);
+            } catch (FileNotFoundException ex) {
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
+                return;
+            }
+        }
+    }
+
+    public boolean validateInput() {
+        if (activityEditProjectBinding.projectTitleBig.getText().toString().isEmpty()) {
+            Toast.makeText(EditProjectActivity.this, "Failed to submit: Empty title", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (activityEditProjectBinding.projectDescription.getText().toString().isEmpty()) {
+            Toast.makeText(EditProjectActivity.this, "Failed to submit: Empty description", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (activityEditProjectBinding.numberOfParticipants.getText().toString().isEmpty()) {
+            Toast.makeText(EditProjectActivity.this, "Failed to submit: Empty number of participants needed", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (activityEditProjectBinding.ppPerParticipant.getText().toString().isEmpty()) {
+            Toast.makeText(EditProjectActivity.this, "Failed to submit: Empty PPs for each participant", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        int numParticipants = Integer.parseInt(activityEditProjectBinding.numberOfParticipants.getText().toString());
+        if (numParticipants <= 0) {
+            Toast.makeText(EditProjectActivity.this, "Failed to submit: Non-positive number of participants needed", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        double ppPerParticipant = Double.parseDouble(activityEditProjectBinding.ppPerParticipant.getText().toString());
+        if (ppPerParticipant < 0) {
+            Toast.makeText(EditProjectActivity.this, "Failed to submit: Negative PPs for each participant", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        double currentPPs = ((Parti) this.getApplication()).getLoggedInUser().getParticipationPoints();
+        if (Parti.calculatePPCost(numParticipants, ppPerParticipant) > currentPPs) {
+            Toast.makeText(EditProjectActivity.this, "Failed to submit: You have insufficient PPs to launch the project", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return true;
     }
 }
