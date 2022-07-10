0b5e8534de18fcfd9df33015bf88c621506a2d38
Felix Huang <6328huang@gmail.com>
2022-07-10 21:42:27 +0800

    improved recycler view list: added a progress bar

--- Parti/app/src/main/java/com/example/parti/Parti.java
+++ Parti/app/src/main/java/com/example/parti/Parti.java
@@ -12,7 +12,7 @@ public class Parti extends Application {
     public static final int SHORT_DESCRIPTION_LENGTH = 300;
     public static final int DEFAULT_RANKING = 0;
     public static final String DEFAULT_PROFILE_IMAGE_ID = "profile_images/default_profile_image.jpg";
-    public static final String DEFAULT_PROJECT_IMAGE_ID = "s0";
+    public static final String DEFAULT_PROJECT_IMAGE_ID = "";
     public static final String DEFAULT_YEAR_OF_MATRIC = "2022";
     public static final String DEFAULT_USER_SELF_DESCRIPTION = "This user hasn't left any self description";
     public static final long DEFAULT_PARTICIPATION_POINTS = 500;
--- Parti/app/src/main/java/com/example/parti/recyclerview/BrowseProjectsAdapter.java
+++ Parti/app/src/main/java/com/example/parti/recyclerview/BrowseProjectsAdapter.java
@@ -1,19 +1,28 @@
 package com.example.parti.recyclerview;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.parti.Parti;
 import com.example.parti.databinding.BrowseProjectsRecyclerViewListItemBinding;
+import com.example.parti.ui.main.MyProfileFragment;
 import com.example.parti.ui.main.ViewProjectActivity;
 import com.example.parti.wrappers.Project;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.time.LocalDateTime;
 import java.util.Locale;
@@ -69,19 +78,53 @@ public class BrowseProjectsAdapter extends FirestoreAdapter<BrowseProjectsAdapte
 
             // Load image
             String imageId = project.getImageId();
-            if (imageId.equals(DEFAULT_PROJECT_IMAGE_ID)) imageId = "" + android.R.drawable.ic_dialog_info;
-            Glide.with(browseProjectsRecyclerViewListItemBinding.projectImage.getContext())
-                    .load(android.R.drawable.ic_dialog_info) //TODO
-                    .into(browseProjectsRecyclerViewListItemBinding.projectImage);
+            if (imageId == null || imageId.equals(DEFAULT_PROJECT_IMAGE_ID)) {
+                Glide.with(browseProjectsRecyclerViewListItemBinding.projectImage.getContext())
+                        .load(android.R.drawable.ic_dialog_info)
+                        .into(browseProjectsRecyclerViewListItemBinding.projectImage);
+            } else {
+                StorageReference imageReference = FirebaseStorage.getInstance().getReference().child(project.getImageId());
+                final long ONE_MEGABYTE = 1024 * 1024;
+                imageReference.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                    @Override
+                    public void onSuccess(byte[] bytes) {
+                        Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                        browseProjectsRecyclerViewListItemBinding.projectImage.setImageBitmap(bmp);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Toast.makeText(ViewHolder.this.itemView.getContext(), "Failed to download project image", Toast.LENGTH_LONG).show();
+                        //If failed, load the default local image;
+                        Glide.with(browseProjectsRecyclerViewListItemBinding.projectImage.getContext())
+                                .load(android.R.drawable.ic_dialog_info)
+                                .into(browseProjectsRecyclerViewListItemBinding.projectImage);
+                    }
+                });
+            }
 
             browseProjectsRecyclerViewListItemBinding.projectTitle.setText(project.getName());
             browseProjectsRecyclerViewListItemBinding.shortDescription.setText(project.getShortDescription());
+            int numParticipants = project.getNumParticipants();
+            int numParticipantsNeeded = project.getNumParticipantsNeeded();
+            browseProjectsRecyclerViewListItemBinding.projectProgressBarSmall.setMax(numParticipantsNeeded);
+            browseProjectsRecyclerViewListItemBinding.projectProgressBarSmall.setProgress(numParticipants);
+            String progress = String.format(Locale.ENGLISH, "%d/%d Participated", numParticipants, numParticipantsNeeded);
+            browseProjectsRecyclerViewListItemBinding.projectProgressTextSmall.setText(progress);
+            float rating = 0;
+            if (numParticipants != 0) rating = ((float) project.getTotalRating()) / numParticipants;
+            browseProjectsRecyclerViewListItemBinding.projectRatingBarSmall.setRating(rating);
+            String preview = String.format(Locale.ENGLISH, "%.1f/5", rating);
+            browseProjectsRecyclerViewListItemBinding.projectRatingPreview.setText(preview);
 
+            /*
             Random rnd = new Random(LocalDateTime.now().toLocalTime().toNanoOfDay());
             float next = rnd.nextFloat() * 5;
             String preview = String.format(Locale.ENGLISH, "%.1f", next);
             browseProjectsRecyclerViewListItemBinding.projectRatingBarSmall.setRating(next); //TODO
             browseProjectsRecyclerViewListItemBinding.projectRatingPreview.setText(preview);
+             */
+
 
             // Click listener
             itemView.setOnClickListener(new View.OnClickListener() {
